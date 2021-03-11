fd0906618dcf9db4255c6f9bb62f94399841d447
undusted <hornespill@gmail.com>
2021-03-12 00:09:10 +0200

    mobile version added

--- app/js/main.js
+++ app/js/main.js
@@ -38,5 +38,11 @@ window.onscroll = function () {
     } 
 
   }
-
+ // On mobile -> nav menu
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
   
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
